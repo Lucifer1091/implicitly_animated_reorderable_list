b43ba4cabe339ff262521df07655d541ddecdf82
AliAman <alirizucharolia@gmail.com>
2023-02-08 12:27:30 +0500

    Bug Fixes

---
+++ example/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:example/ui/ui.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Implicitly Animated Reorderable List Example',
+      theme: ThemeData.light().copyWith(
+        accentColor: Colors.amber.shade800,
+        dividerTheme: DividerThemeData(
+          thickness: 1,
+          color: Colors.grey.shade300,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const LanguagePage(),
+    );
+  }
+}
